b0b45ec1bf1d388ebf4bde043c4d89b80c2e189e
Aeybel Varghese <aeybelvarghese@gmail.com>
2025-02-17 00:49:47 -0700

    Can poll device and return to IIO channel
    A implemented serial callback that fills up a frame of measurement data.
    Reading a IIO channel will return the value from the poll operation

--- tf_luna.h
+++ tf_luna.h
@@ -1,6 +1,7 @@
 #ifndef __TF_LUNA
 #define __TF_LUNA
 
+#include <linux/completion.h>
 #include <linux/iio/iio.h>
 #include <linux/mutex.h>
 #include <linux/of.h>
@@ -8,6 +9,7 @@
 #include <linux/types.h>
 
 #define TF_LUNA_COMMAND_HEADER 0x5A
+#define TF_LUNA_MEASUREMENT_HEADER 0x5959
 #define TF_LUNA_MAX_SEND_BUFFER_SIZE 32
 #define TF_LUNA_TIMEOUT msecs_to_jiffies(10000)
 
@@ -16,12 +18,33 @@
 #define SERDEV_DRIVER_NAME "tf_luna-serdev"
 #define TF_LUNA_DEFAULT_BAUDRATE 115200
 
+typedef struct tf_luna_serial_measurement
+{
+    u8 Dist_L;
+    u8 Dist_H;
+    u8 Amp_L;
+    u8 Amp_H;
+    u8 Temp_L;
+    u8 Temp_H;
+    u8 Checksum;
+} tf_luna_serial_measurement;
+
+struct tf_luna_frame
+{
+    u8 data[TF_LUNA_MAX_SEND_BUFFER_SIZE];
+    u16 expected_length;
+    u16 length;
+};
+
 // Device Information
 struct tf_luna_sensor
 {
     struct serdev_device *serdev;
     struct mutex lock;
     bool driver_init;
+    struct tf_luna_frame frame;
+    struct completion frame_ready;
+    bool configured;
     int baudrate;
     bool trigger_mode;
     int sampling_divisor;
@@ -42,6 +65,7 @@ typedef enum
     ID_GET_VERSION = 0x01,
     ID_SOFT_RESET,
     ID_SAMPLE_FREQ,
+    ID_SAMPLE_TRIG,
 } luna_cmd_id_t;
 
 int tf_luna_probe(struct iio_dev *indio_dev);
